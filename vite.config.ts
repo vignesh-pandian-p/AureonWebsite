edeb14b0f3d51b10e4e7292d0972a907ead7e305
Vignesh_Pandian <55983800+vignesh-pandian-p@users.noreply.github.com>
2025-06-16 08:38:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/AureonWebsite/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -25,7 +26,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "client/dist"),
     emptyOutDir: true,
   },
   server: {
